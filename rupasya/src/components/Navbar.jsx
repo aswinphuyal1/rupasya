91cf4e8698607bfac0ca631b2abde8f7d4d5a6d2
aswinphuyal1 <phuyalaswin651@gmail.com>
2025-06-08 20:12:46 +0545

    drop down menu at profile

--- rupasya/src/components/Navbar.jsx
+++ rupasya/src/components/Navbar.jsx
@@ -12,28 +12,49 @@ const Navbar = () => {
       <ul className="hidden sm:flex gap-5 text-sm text-gray-700">
         <NavLink to="/" class="flex flex-col item-center gap-1">
           <p>HOME</p>
-          <hr className="w-2/4 mx-auto border-none h-[1px] bg-gray-700" hidden />
+          <hr
+            className="w-2/4 mx-auto border-none h-[1px] bg-gray-700"
+            hidden
+          />
         </NavLink>
         <NavLink to="/collection" class="flex flex-col item-center gap-1">
           <p>COLLECTION</p>
-          <hr className="w-2/4 mx-auto border-none h-[1px] bg-gray-700" hidden />
+          <hr
+            className="w-2/4 mx-auto border-none h-[1px] bg-gray-700"
+            hidden
+          />
         </NavLink>
         <NavLink to="/about" class="flex flex-col item-center gap-1">
           <p>ABOUT</p>
-          <hr className="w-2/4 mx-auto border-none h-[1px] bg-gray-700" hidden />
+          <hr
+            className="w-2/4 mx-auto border-none h-[1px] bg-gray-700"
+            hidden
+          />
         </NavLink>
         <NavLink to="/contact" class="flex flex-col item-center gap-1">
           <p>CONTACT</p>
-          <hr className="w-2/4 mx-auto border-none h-[1px] bg-gray-700" hidden />
+          <hr
+            className="w-2/4 mx-auto border-none h-[1px] bg-gray-700"
+            hidden
+          />
         </NavLink>
       </ul>
-      <div className='flex item-center gap-6'>
-        <img src={searchicon} className='w-7 cursor-pointer' alt=''/>
 
-<div className='grou[ relative'>
-  <img  className='w-7 cursor-pointer' src={profileicon} alt=''/>
-</div>
+      <div className="flex item-center gap-6">
+        <img src={searchicon} className="w-7 cursor-pointer" alt="" />
 
+        <div className="group relative">
+          <img className="w-7 cursor-pointer" src={profileicon} alt="" />
+
+          <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4">
+            <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded ">
+              <p className="cursor-pointer hover:text-black">My Profile</p>
+              <p className="cursor-pointer hover:text-black">Order</p>
+              <p className="cursor-pointer hover:text-black">Logout</p>
+
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
