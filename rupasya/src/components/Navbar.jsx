d235c6748dd6947396a2ad7648df72fac762e8c1
aswinphuyal1 <phuyalaswin651@gmail.com>
2025-06-08 20:01:41 +0545

    profile added

--- rupasya/src/components/Navbar.jsx
+++ rupasya/src/components/Navbar.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import  logo  from '../assets/logo.png'
 import {NavLink} from "react-router-dom"
  import logo2 from "../assets/logo2.png" 
+ import searchicon from "../assets/searchico.png"
+ import profileicon from "../assets/prrofile.jpg"
 const Navbar = () => {
   return (
     <div className="flex items-center justify-between py-5 font-medium">
@@ -25,6 +27,14 @@ const Navbar = () => {
           <hr className="w-2/4 mx-auto border-none h-[1px] bg-gray-700" hidden />
         </NavLink>
       </ul>
+      <div className='flex item-center gap-6'>
+        <img src={searchicon} className='w-7 cursor-pointer' alt=''/>
+
+<div className='grou[ relative'>
+  <img  className='w-7 cursor-pointer' src={profileicon} alt=''/>
+</div>
+
+      </div>
     </div>
   );
 }
