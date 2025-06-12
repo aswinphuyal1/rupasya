270c7fb3fca10556a4f1e4ac831520c466e0439e
aswinphuyal1 <phuyalaswin651@gmail.com>
2025-06-12 23:02:39 +0545

    slogon added

--- rupasya/src/components/Latestcolledtion.jsx
+++ rupasya/src/components/Latestcolledtion.jsx
@@ -6,13 +6,16 @@ const Latestcolledtion = () => {
 
     const {products}= useContext(Shopcontext);
   return (
-    <div className='my-10'>
-      <div className='text-center py-8 text-3xl'>
-<Title text1={'LATEST'} text2={'COLLECTION'}  />
+    <div className="my-10">
+      <div className="text-center py-8 text-3xl">
+        <Title text1={"LATEST"} text2={"COLLECTION"} />
+        <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600">
+          "Crafted with care, inspired by nature – beauty that lets your skin
+          breathe and your soul glow."
+        </p>
       </div>
-      
     </div>
-  )
+  );
 }
 
 export default Latestcolledtion
