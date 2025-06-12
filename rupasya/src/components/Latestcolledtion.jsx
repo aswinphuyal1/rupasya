808791093d7112a7bd388d880af1e84cfd3533e9
aswinphuyal1 <phuyalaswin651@gmail.com>
2025-06-12 23:59:10 +0545

    product iteam created

--- rupasya/src/components/Latestcolledtion.jsx
+++ rupasya/src/components/Latestcolledtion.jsx
@@ -1,11 +1,19 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { Shopcontext } from '../context/Shopcontext'
 import Title from './Title';
 
 const Latestcolledtion = () => {
 
     const {products}= useContext(Shopcontext);
-  return (
+    const [latestproduct,setlatestproduct]=useState([])
+useEffect(()=>
+{
+
+  setlatestproduct(products.slice(0,10));
+
+
+},[])
+    return (
     <div className="my-10">
       <div className="text-center py-8 text-3xl">
         <Title text1={"LATEST"} text2={"COLLECTION"} />
@@ -14,6 +22,9 @@ const Latestcolledtion = () => {
           breathe and your soul glow."
         </p>
       </div>
+
+
+
     </div>
   );
 }
---
+++ rupasya/src/components/Productitem.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Productitem = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Productitem
