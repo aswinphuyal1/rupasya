a16cc23a314acc5c2bfc32837cad6161abffd58d
aswinphuyal1 <phuyalaswin651@gmail.com>
2025-06-12 22:47:43 +0545

    title added

--- rupasya/src/components/Latestcolledtion.jsx
+++ rupasya/src/components/Latestcolledtion.jsx
@@ -1,11 +1,15 @@
 import React, { useContext } from 'react'
 import { Shopcontext } from '../context/Shopcontext'
+import Title from './Title';
 
 const Latestcolledtion = () => {
 
     const {products}= useContext(Shopcontext);
   return (
-    <div>
+    <div className='my-10'>
+      <div className='text-center py-8 text-3xl'>
+<Title text1={'LATEST'} text2={'COLLECTION'}  />
+      </div>
       
     </div>
   )
--- rupasya/src/context/Shopcontext.jsx
+++ rupasya/src/context/Shopcontext.jsx
@@ -1,20 +1,17 @@
 import { createContext } from "react";
-import {product} from "../assets/"
- export const Shopcontext = createContext();
- const Shopcontextprovider = (props) =>
- {
+//import {product} from "../assets/"
+export const Shopcontext = createContext();
+const Shopcontextprovider = (props) => {
+  const currency = "$";
+  const delivery_fee = 10;
 
-    const currency ='$'
-    const delivery_fee=10;
-
-    const value ={
-product,currency,delivery_fee
-    }
-    return (
-        <Shopcontext.Provider value={value}>
-{props.children}
-
-        </Shopcontext.Provider>
-    )
- }
- export default Shopcontextprovider;
+  const value = {
+  //product,
+    currency,
+    delivery_fee,
+  };
+  return (
+    <Shopcontext.Provider value={value}>{props.children}</Shopcontext.Provider>
+  );
+};
+export default Shopcontextprovider;
