8cf62b01666bccd5d5a71d70f3ffb8ccc91924ec
aswinphuyal1 <phuyalaswin651@gmail.com>
2025-06-15 19:03:19 +0545

    ONSUBMMITE HANDELER ADDED

--- rupasya/src/components/Newsletter.jsx
+++ rupasya/src/components/Newsletter.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 
 const Newsletter = () => {
+    const onubmithandler=(event)=>{
+event.preventDefault()
+    }
+
   return (
     <div className="text-center">
       <p className="text-2xl font-medium text-gray-800">
@@ -9,7 +13,7 @@ const Newsletter = () => {
       <p className="text-gray-400 mt-3 ">
         This is some light gray text with a top margin.
       </p>
-      <form  className='w-full sm:w-1/2 flex items-center gap-3 mx-auto my-6 border pl-3'>
+      <form onSubmit={onubmithandler} className='w-full sm:w-1/2 flex items-center gap-3 mx-auto my-6 border pl-3'>
         <input className='w-full sm:flex-1 outline-none' type="email" placeholder='Enter your email' required/>
         <button type='submit' className='bg-black text-white text-xs px-10 py-4'>SUBSCRIBE</button>
       </form>
