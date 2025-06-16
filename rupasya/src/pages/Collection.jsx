ae88beddf26658cc15b259df15cb00e0df01433a
aswinphuyal1 <phuyalaswin651@gmail.com>
2025-06-16 21:31:39 +0545

    filter section done

--- rupasya/src/pages/Collection.jsx
+++ rupasya/src/pages/Collection.jsx
@@ -1,6 +1,6 @@
-import React, { useContext, useState } from 'react'
-import { Shopcontext } from '../context/Shopcontext'
-
+import React, { useContext, useState } from "react";
+import { Shopcontext } from "../context/Shopcontext";
+import drop from "../assets/drop.png";
 const products = [
   {
     id: 1,
@@ -19,14 +19,20 @@ const products = [
 ];
 
 const Collection = () => {
-  const {product} =useContext(Shopcontext)
-  const [showfilter,setshowfilter]=useState(false)
+  const { product } = useContext(Shopcontext);
+  const [showfilter, setshowfilter] = useState(false);
   return (
     <div className="flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t ">
       {/* filter*/}
       <div className="min-w-60">
-        <p className="my-2 text-xl flex items-center cusrsor-pointer gap-2">
-          FILTERS
+        <p className="my-2 text-xl flex items-center cursor-pointer gap-2">
+          Filters
+          <img
+            onClick={() => setshowfilter(!showfilter)}
+            className={`h-3 sm:hidden ${showfilter ? "rotate-90" : ""}`}
+            src={drop}
+            alt="/"
+          />
         </p>
         {/*category filter */}
         {/* <div className={`border border-gray-300 pl-5 py-3 mt-6 ${showfilter ? ' ':'hidden'}sm:block`}> */}
@@ -37,33 +43,62 @@ const Collection = () => {
         >
           <p className="mb-3 text-sm font-medium">CATEGORY</p>
           <div className="flex flex-col gap-2 text-sm font-light text-gray-700">
-            <p className="flex flex gap-2">
+            <label className="flex items-center gap-2">
               <input
                 className="w-3"
                 type="checkbox"
                 value={"Makeup Products"}
               />
               Makeup Products
-            </p>
+            </label>
 
-            <p className="flex gap-2">
+            <label className="flex items-center gap-2">
               <input
                 className="w-3"
                 type="checkbox"
                 value={"Skincare Products"}
               />
               Skincare Products
+            </label>
+
+            <label className="flex items-center gap-2">
+              <input className="w-3" type="checkbox" value={"Lip & Eye Care"} />
+              Lip & Eye Care
+            </label>
+          </div>
+        </div>
+        {/* sub catogery*/}
+
+        <div
+          className={`border border-gray-300 pl-5 py-3 my-5 ${
+            showfilter ? "" : "hidden"
+          } sm:block`}
+        >
+          <p className="mb-3 text-sm font-medium">SUB CATEGORY</p>
+          <div className="flex flex-col gap-2 text-sm font-light text-gray-700">
+            <p className="flex flex gap-2">
+              <input className="w-3" type="checkbox" value={"MEN"} />
+              MEN
             </p>
 
             <p className="flex gap-2">
-              <input className="w-3" type="checkbox" value={"Lip & Eye Care"} />
-              Lip & Eye Care
+              <input className="w-3" type="checkbox" value={"WOMEN"} />
+              WOMEN
+            </p>
+
+            <p className="flex gap-2">
+              <input className="w-3" type="checkbox" value={"PREMIUM"} />
+              PREMIUM
+            </p>
+            <p className="flex gap-2">
+              <input className="w-3" type="checkbox" value={"BASIC"} />
+              BASIC
             </p>
           </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
-export default Collection
+export default Collection;
