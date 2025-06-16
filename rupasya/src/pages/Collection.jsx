233b79a7b0b25bf280d14d2819280a648da24db8
aswinphuyal1 <phuyalaswin651@gmail.com>
2025-06-16 20:08:15 +0545

    collection section

--- rupasya/src/pages/Collection.jsx
+++ rupasya/src/pages/Collection.jsx
@@ -1,11 +1,50 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import { Shopcontext } from '../context/Shopcontext'
+
+const products = [
+  {
+    id: 1,
+    name: "Lipstick",
+    price: 15,
+    image: "/src/assets/makeup.png",
+    Bestsheller: true,
+  },
+  {
+    id: 2,
+    name: "Foundation",
+    price: 25,
+    image: "/src/assets/product.jpg",
+    Bestsheller: false,
+  },
+];
 
 const Collection = () => {
+  const {product} =useContext(Shopcontext)
+  const [showfilter,setshowfilter]=useState(false)
   return (
-    <div>
-      
+    <div className="flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t ">
+      {/* filter*/}
+      <div className="min-w-60">
+        <p className="my-2 text-xl flex items-center cusrsor-pointer gap-2">
+          FILTERS
+        </p>
+        {/*category filter */}
+        {/* <div className={`border border-gray-300 pl-5 py-3 mt-6 ${showfilter ? ' ':'hidden'}sm:block`}> */}
+        <div
+          className={`border border-gray-300 pl-5 py-3 mt-6 ${
+            showfilter ? "" : "hidden"
+          } sm:block`}
+        >
+          <p className="mb-3 text-sm font-medium">CATEGORY</p>
+          <div className="flex felx-col gap-2 text-sm font-light text-gray-700">
+            <p className="flex gap-2">
+              <input type="checkbox" />
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Collection
