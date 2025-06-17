29b1892dd614e1dd0b8f5344c3bb2df207f0e49f
aswinphuyal1 <phuyalaswin651@gmail.com>
2025-06-17 19:51:35 +0545

    filter and collection added

--- rupasya/src/pages/Collection.jsx
+++ rupasya/src/pages/Collection.jsx
@@ -1,6 +1,8 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Shopcontext } from "../context/Shopcontext";
 import drop from "../assets/drop.png";
+import Title from "../components/Title";
+import Productitem from "../components/Productitem";
 const products = [
   {
     id: 1,
@@ -19,17 +21,30 @@ const products = [
 ];
 
 const Collection = () => {
-  const { product } = useContext(Shopcontext);
+  const { products } = useContext(Shopcontext);
   const [showfilter, setshowfilter] = useState(false);
+  const [filterproduct, setfilterproduct] = useState([]);
+  const [category,setcategory]=useState([])
+  const [subcategory,setsubcategory]= useState([]);
+
+  
+
+  useEffect(() => {
+    setfilterproduct(products);
+  }, []);
+
   return (
     <div className="flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t ">
       {/* filter*/}
       <div className="min-w-60">
-        <p className="my-2 text-xl flex items-center cursor-pointer gap-2">
+        <p
+          onClick={() => setshowfilter(!showfilter)}
+          className="my-2 text-xl flex items-center cursor-pointer gap-2"
+        >
           Filters
           <img
             onClick={() => setshowfilter(!showfilter)}
-            className={`h-3 sm:hidden ${showfilter ? "rotate-90" : ""}`}
+            className={`h-3 sm:hidden ${showfilter ? "rotate-360" : ""}`}
             src={drop}
             alt="/"
           />
@@ -76,7 +91,7 @@ const Collection = () => {
         >
           <p className="mb-3 text-sm font-medium">SUB CATEGORY</p>
           <div className="flex flex-col gap-2 text-sm font-light text-gray-700">
-            <p className="flex flex gap-2">
+            <p className="flex gap-2">
               <input className="w-3" type="checkbox" value={"MEN"} />
               MEN
             </p>
@@ -95,6 +110,31 @@ const Collection = () => {
               BASIC
             </p>
           </div>
+        </div>
+      </div>
+
+      {/*Right side*/}
+      <div className="flex-1">
+        <div className="flex justify-between text-base sm:text-2xl mb-4">
+          <Title text1={"ALL"} text2={"COLLECTION"} />
+          {/* Product sort*/}
+          <select className="border-2 border-gray-300 text-sm px-2">
+            <option value="relavent">Relavent</option>
+            <option value="low-high">Low to high</option>
+            <option value="high to low">High to low</option>
+          </select>
+        </div>
+        {/*MAPPING OF PRODUCTS */}
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 gap-y-6">
+          {filterproduct.map((iteam, index) => (
+            <Productitem
+              key={index}
+              id={iteam.id}
+              image={iteam.image}
+              name={iteam.name}
+              price={iteam.price}
+            />
+          ))}
         </div>
       </div>
     </div>
