de347517cc05b99d954024004988aff9c8408dca
aswinphuyal1 <phuyalaswin651@gmail.com>
2025-06-21 17:26:55 +0545

    product detalis showing in console

--- rupasya/src/pages/Poduct.jsx
+++ rupasya/src/pages/Poduct.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import { Shopcontext } from '../context/Shopcontext';
 
 const Poduct = () => {
   const { productID} = useParams();
-  console.log(productID)
+  const{products}=useContext(Shopcontext)
+  const[productdata,setproductdata]=useState(false);
+  
+  const fetchproductdata = async () => {
+    products.map((item)=>
+    {
+      if(item.id==productID)
+      {
+        setproductdata(item);
+        console.log(item);
+        return null;
+      }
+    })
+  }
+
+  useEffect(()=>
+  {
+    fetchproductdata();
+  },[productID,products])
+
+
   return (
     <div>
       
