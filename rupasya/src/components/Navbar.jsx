d96d2a761c0ad788026ca6d96edaa0f35ccb85c0
aswinphuyal1 <phuyalaswin651@gmail.com>
2025-06-23 23:18:53 +0545

    login page done

--- rupasya/src/components/Navbar.jsx
+++ rupasya/src/components/Navbar.jsx
@@ -59,7 +59,9 @@ const Navbar = () => {
           alt=""
         />
         <div className="group relative">
-          <img className="w-5 cursor-pointer" src={profileicon} alt="" />
+          <Link to='/login'>
+            <img className="w-5 cursor-pointer" src={profileicon} alt="" />
+          </Link>
           <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4">
             <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-700 rounded ">
               <p className="cursor-pointer hover:text-black">My Profile</p>
--- rupasya/src/pages/Login.jsx
+++ rupasya/src/pages/Login.jsx
@@ -1,11 +1,70 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Login = () => {
+  const [currentstate, setcurrentstate] = useState("login");
+  const onsubmithandler = async (event) => {
+    event.preventDefault();
+    //yeslay relode huna dinna
+  };
   return (
     <div>
-      
-    </div>
-  )
-}
+      <form
+        onSubmit={onsubmithandler}
+        className="flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14  gap-4 text-gray-80 "
+      >
+        <div className="inline-flex items-center gap-2 mb-2 mt-10">
+          <p className="prata-regular text-3xl">
+            {currentstate === "login" ? "Login" : "Sign Up"}
+          </p>
+          <hr className="border-none h-[1.5px] w-8 bg-gray-800"></hr>
+        </div>
+        {currentstate === "login" ? (
+          ""
+        ) : (
+          <input
+            type="text"
+            className="w-full px-3 py-2 border border-gray-800 "
+            placeholder="Enter your name"
+            required
+          ></input>
+        )}
+        <input
+          type="email"
+          className="w-full px-3 py-2 border border-gray-800 "
+          placeholder="Email"
+          required
+        ></input>
+        <input
+          type="password"
+          className="w-full px-3 py-2 border border-gray-800 "
+          placeholder="Password"
+          required
+        ></input>
+        <div className="w-full flex justify-between text-sm mt-[-8px]">
+          <p className="cursor-pointer">Forget your password</p>
+          {currentstate === "login" ? (
+            <p
+              onClick={() => setcurrentstate("signup")}
+              className="cursor-pointer"
+            >
+              create account
+            </p>
+          ) : (
+            <p
+              onClick={() => setcurrentstate("login")}
+              className="cursor-pointer"
+            >
+              Login
+            </p>
+          )}
+        </div>
 
-export default Login
+        <button className="bg-black text-white font-light px-8 py-2 mt-4">
+          {currentstate === "login" ? "Sign In" : "Sign Up"}
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
