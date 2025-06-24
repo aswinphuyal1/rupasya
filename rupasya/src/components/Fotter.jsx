58b9a33df29ee704b1f2863aa842164eccb1bf03
aswinphuyal1 <phuyalaswin651@gmail.com>
2025-06-24 08:42:26 +0545

    open ai api

--- rupasya/src/components/Fotter.jsx
+++ rupasya/src/components/Fotter.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import logo2 from "../assets/logo2.png";
-
+import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <div className="mt-40 my-10 text-sm">
       <div className="flex flex-col sm:grid sm:grid-cols-[3fr_1fr_1fr] gap-14">
         {/* Logo Section */}
         <div>
-          <img src={logo2} alt="Brand Logo" className="mb-5 w-32" />
+          <Link to="/">
+            {" "}
+            <img src={logo2} alt="Brand Logo" className="mb-5 w-32" />
+          </Link>
           <p className="w-full md:w-2/3 text-gray-600">
             **Rupasya** is a proudly Nepali makeup brand offering high-quality,
             affordable beauty products tailored to local skin tones and
@@ -20,10 +23,22 @@ const Footer = () => {
         <div>
           <p className="text-xl font-medium mb-3">COMPANY</p>
           <ul className="flex flex-col gap-1 text-gray-600">
-            <li typeof="./" >Home</li>
-            <li>About us</li>
-            <li>Delivery</li>
-            <li>Privacy policy</li>
+            <Link to="">
+              {" "}
+              <li>Home</li>
+            </Link>
+            <Link to="/about">
+              {" "}
+              <li>About us</li>
+            </Link>
+            <Link to="/cart">
+              {" "}
+              <li>Delivery</li>
+            </Link>
+            <Link to="/contact" >
+              {" "}
+              <li>Privacy policy</li>
+            </Link>
           </ul>
         </div>
         <div>
