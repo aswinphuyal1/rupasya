d5f2c6499438958dc4c24d2d28488041e3459d1e
aswinphuyal1 <phuyalaswin651@gmail.com>
2025-06-25 15:18:16 +0545

    connection establishment

---
+++ backend/config/mongodb.js
@@ -0,0 +1,13 @@
+import mongoose, { Mongoose } from "mongoose";
+
+const connectdb = async()=>
+{
+mongoose.connection.on('connected',()=>
+{
+   console.log("db connected") 
+}
+)
+    await mongoose.connect(`${process.env.MONGODB_url}/rupasya`);
+
+}
+export default connectdb
