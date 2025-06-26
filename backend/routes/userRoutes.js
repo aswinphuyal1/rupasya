6a99aca3a9598d20cdc278ee406090989b18a572
aswinphuyal1 <phuyalaswin651@gmail.com>
2025-06-26 15:41:07 +0545

    corrected an error in user models

--- backend/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { registeruser } from "../controllers/uercontoller.js";
-
-const router = Router();
-
-router.post("/registeruser", registeruser);
-
-export default router;
