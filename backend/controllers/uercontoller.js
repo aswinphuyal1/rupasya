634f6b7d18636096303826a15f5a1eccf3b5184b
aswinphuyal1 <phuyalaswin651@gmail.com>
2025-06-26 15:43:51 +0545

    usercontoller working

---
+++ backend/controllers/uercontoller.js
@@ -0,0 +1,56 @@
+import validator from "validator";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import usermodel from "../models/usermodel.js";
+
+//to generate the token
+const createtoken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET);
+};
+
+//route for user login
+const loginuser = async (req, res) => {};
+
+//route for user register
+
+const registeruser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    //checkingg user already exist or not
+    const exists = await usermodel.findOne({ email });
+    if (exists) {
+      return res.json({ success: false, message: "user already exist" });
+    }
+    //check valadiation emailforamat and strong password
+    if (!validator.isEmail(email)) {
+      return res.json({
+        success: false,
+        message: "please enter a valid email",
+      });
+    }
+    if (password.length < 8) {
+      return res.json({ success: false, message: "Enter the strong password" });
+    }
+    //hashing user passeord
+    const salt = await bcrypt.genSalt(10);
+    const hashedpassword = await bcrypt.hash(password, salt);
+    const newuser = new usermodel({
+      name,
+      email,
+      password: hashedpassword,
+    });
+    //data base ma store hunxa
+    const user = await newuser.save();
+    //user can  login
+    const token = createtoken(user.id);
+    res.json({ success: true, token });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+//for admin login
+const adminlogin = async (req, res) => {};
+
+export { loginuser, registeruser, adminlogin };
