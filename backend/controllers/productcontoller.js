7e0d71b81ae563d91083836cdeb8bfa1a6d6d85a
aswinphuyal1 <phuyalaswin651@gmail.com>
2025-06-26 21:00:24 +0545

    image saved

--- backend/controllers/productcontoller.js
+++ backend/controllers/productcontoller.js
@@ -1,5 +1,5 @@
 //function for add product
-
+import { v2 as cloudinary } from "cloudinary";
 const addproduct = async (req, res) => {
   try {
     const {
@@ -19,19 +19,20 @@ const addproduct = async (req, res) => {
     const image3 = (req.files && req.files?.image3?.[0]) || null;
     const image4 = (req.files && req.files?.image4?.[0]) || null;
 
-    console.log(
-      name,
-      description,
-      price,
-      image,
-      category,
-      subcategory,
-      sizes,
-      bestseller,
-      date
+     const images = [image1, image2, image3, image4].filter(
+      (iteam) => iteam !== undefined
     );
-    console.log(image1, image2, image3, image4);
-    res.json({});
+
+   const imageurl =await Promise.all(
+    images.map(async(iteam)=>
+    {
+      const result =await cloudinary.uploader.upload(iteam.path,{resource_type:'image'})
+      return result.secure_url;
+    })
+   )
+
+    console.log(imageurl);
+    res.json({ success: true });
   } catch (error) {
     console.log(error);
     res.json({ success: false, message: "error" });
