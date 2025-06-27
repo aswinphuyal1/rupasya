a40fac4589b9710d9fe5df4a3b38314d7395f96e
aswinphuyal1 <phuyalaswin651@gmail.com>
2025-06-27 15:59:19 +0545

    navbar added

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import logo from "../assets/logo2.png";
 const Navbar = () => {
   return (
-    <div>
+    <div className='flex items-center py-2 px-[4%] justify-between'>
       <img className='w-[max(15%,80px)]' src={logo} alt='' />
-      <button>Logout</button>
+      <button className='bg-gray-600 text-white px-5 py-2 sm:py-2 rounded-full text-xs sm:text-sm'>Logout</button>
     </div>
   )
 }
