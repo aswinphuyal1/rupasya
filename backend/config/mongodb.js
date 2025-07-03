9925fb487c860360c1f0a2a675839f424e477db6
aswinphuyal1 <phuyalaswin651@gmail.com>
2025-07-04 00:40:54 +0545

    backeup added

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,24 +1,50 @@
-import mongoose from "mongoose";
+import  mongoose  from "mongoose";
 
 const connectdb = async () => {
   mongoose.connection.on("connected", () => {
     console.log("db connected");
   });
-
-  mongoose.connection.on("error", (err) => {
-    console.error("MongoDB connection error:", err);
-  });
-
-  try {
-    await mongoose.connect(`${process.env.MONGODB_url}/rupasya`, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      serverSelectionTimeoutMS: 15000, // increase timeout
-      socketTimeoutMS: 20000, // increase socket timeout
-    });
-  } catch (err) {
-    console.error("Failed to connect to MongoDB:", err);
-    process.exit(1);
-  }
+  await mongoose.connect(`${process.env.MONGODB_url}/rupasya`);
 };
 export default connectdb;
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import mongoose from "mongoose";
+
+// const connectdb = async () => {
+//   mongoose.connection.on("connected", () => {
+//     console.log("db connected");
+//   });
+
+//   mongoose.connection.on("error", (err) => {
+//     console.error("MongoDB connection error:", err);
+//   });
+
+//   try {
+//     await mongoose.connect(`${process.env.MONGODB_url}/rupasya`, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//       serverSelectionTimeoutMS: 15000, // increase timeout
+//       socketTimeoutMS: 20000, // increase socket timeout
+//     });
+//   } catch (err) {
+//     console.error("Failed to connect to MongoDB:", err);
+//     process.exit(1);
+//   }
+// };
+// export default connectdb;
